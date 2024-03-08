d4ef92e481f85c926c24d4b16ab55f1fb2c4098b
ruzaanistudio <fernando.romay@ruzaani.com>
2024-03-08 17:00:20 -0600

    Editada dirección de la empresa

--- common/contact-data.php
+++ common/contact-data.php
@@ -7,13 +7,13 @@
 $dataPhone = '9564146657';
 $dataPhoneStr = '956 414 6657';
 $dataWhatsapp = '19564146657';
-$dataEmail = 'sales@tagac.com.mx';
+$dataEmail = 'sales@tagac.com';
 
-$streetAddress = 'Ángel Martínez 637';
-$addressLocality = 'Monterrey';
-$addressRegion = 'Nuevo León';
-$postalCode = '64030';
-$addressCountry = 'Mexico';
+$streetAddress = '801 Bona Terra Dr SUIT 190';
+$addressLocality = 'Phar';
+$addressRegion = 'Texas';
+$postalCode = '78577';
+$addressCountry = 'United States';
 $dataAddress = '<b>Houston</b><br>6807 Wynnwood Ln, Houston, TX 77008</p>
 <p><b>Phar</b><br>9801 S Keystone Dr, Phar, TX 78577';
 $dataAddressFoot = '<b>Houston: </b><br>6807 Wynnwood Ln, TX 77008</p>
